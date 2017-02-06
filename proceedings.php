2e687c168c3fcc241fedad49e131f5873eec1108
Devin Cremins <drcremin@ncsu.edu>
2017-02-06 10:16:06 -0500

    take 1 million

--- proceedings.php
+++ proceedings.php
@@ -11,10 +11,10 @@ Author URI: http://octopusoddments.com
 
 // Add all files in lib folder into array
 $include = [
-  '/lib/cpt.php',               // Register Post Type
-  '/lib/acf.php',               // Register Fields
-  '/lib/templates.php',         // Register Views
-  '/lib/author-filter.php'      // Alter Co-Author List Display
+  '/app/lib/cpt.php',               // Register Post Type
+  '/app/lib/acf.php',               // Register Fields
+  '/app/lib/templates.php',         // Register Views
+  '/app/lib/author-filter.php'      // Alter Co-Author List Display
 ];
 
 // Require Once each file in the array
@@ -29,14 +29,14 @@ unset($file, $filepath);
 
 /* Add main.css */
 add_action('wp_enqueue_scripts', function(){
-	wp_enqueue_style( 'proceedings_css', plugins_url( '/styles/main.css', __FILE__ ) );
+	wp_enqueue_style( 'proceedings_css', plugins_url( '/app/styles/main.css', __FILE__ ) );
 });
 
 
 /* Script-tac-ulous -> tab scripts */
 add_action( 'wp_enqueue_scripts', function() {
- wp_enqueue_script( 'tabs' , plugins_url( '/js/jquery.tabslet.min.js', __FILE__ ) , array( 'jquery' ), '1', true );
- wp_enqueue_script( 'tabs-init' , plugins_url( '/js/tab-init.js', __FILE__ ), array( 'tabs' ), '1', true );
+ wp_enqueue_script( 'tabs' , plugins_url( '/app/js/jquery.tabslet.min.js', __FILE__ ) , array( 'jquery' ), '1', true );
+ wp_enqueue_script( 'tabs-init' , plugins_url( '/app/js/tab-init.js', __FILE__ ), array( 'tabs' ), '1', true );
 });
 
 ?>
