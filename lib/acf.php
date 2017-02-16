044e36d66dcfed4ef6b512bb706f7ed0b8a730c2
Devin Cremins <drcremin@ncsu.edu>
2017-02-16 15:32:08 -0500

    fix ACF includes

--- lib/acf.php
+++ lib/acf.php
@@ -2,7 +2,7 @@
 
 //Include the /acf folder in the places to look for ACF Local JSON files
 add_filter('acf/settings/load_json', function ($paths) {
-    $paths[] = dirname(__FILE__) . '/acf';
+    $paths[] = dirname(__FILE__) . './../acf';
     return $paths;
 });
 
