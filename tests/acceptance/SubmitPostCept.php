6f83e3a42ae16659fcf7831e3f4886885471d3c8
Devin Cremins <drcremin@ncsu.edu>
2017-02-16 11:58:02 -0500

    recorder

--- tests/acceptance/SubmitPostCept.php
+++ tests/acceptance/SubmitPostCept.php
@@ -8,35 +8,27 @@ $I->comment($notAdmin);
 
 $I->amGoingTo('log in as an editor');
 $I->loginAs('AcceptanceTester', 'newTest');
-$I->makeScreenshot('dashboard');
 $I->dontSee('ERROR');
 $I->see('Dashboard', 'h1');
 
 $I->amGoingTo('create a new proceeding');
 $I->see('Proceedings');
-$I->makeScreenshot('proceeding');
 $I->click('Proceedings');
-$I->makeScreenshot('proceeding2');
 $I->click('Proceedings');
-$I->makeScreenshot('proceeding3');
 $I->see('Proceedings', 'h1');
 $I->see('Add New', '//*[@id="wpbody-content"]/div[3]/a');
 $I->click('//*[@id="wpbody-content"]/div[3]/a');
-$I->makeScreenshot('add new');
 $I->fillField('Enter title here', 'newTest');
+$I->seeElement('content');
 $I->fillField('content', 'Test content! Imagine this is an abstract.');
 $I->selectOption('Session', '101');
-$I->seeElement('content');
 $I->fillField('Session Date', '2/16/17');
 $I->selectOption('//*[@id="acf-field_588fa508b8276"]', '202');
 $I->fillField('Speaker', 'Bob Schwartzman');
-$I->makeScreenshot('fields');
 
 $I->amGoingTo('publish and view a new proceeding');
 $I->click('#publish');
-$I->makeScreenshot('published');
 $I->see('Post published');
 $I->expect('proceeding is available on site');
 $I->click('View post');
-$I->makeScreenshot('content');
 $I->see('Test content');
