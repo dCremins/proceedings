a7f1acae8b2151829cea5c3a1b2ca1bfcaa7104f
Devin Cremins <drcremin@ncsu.edu>
2017-03-24 10:14:37 -0400

    update archive sorting

--- lib/archive-proceedings.php
+++ lib/archive-proceedings.php
@@ -5,13 +5,45 @@
  */
         // get posts
         $tabs = [];
+
+        $sessions_query = new WP_Query(array(
+        'post_type'         => 'session',
+        'posts_per_page'    => -1,
+        'order'             => 'ASC',
+        'meta_query' => array(
+            'relation' => 'AND',
+            'start' => array(
+                'key' => 'start_time',
+                'compare' => 'EXISTS',
+            ),
+            'end' => array(
+                'key' => 'end_time',
+                'compare' => 'EXISTS',
+            ),
+        ),
+        'orderby' => array(
+            'end' => 'ASC',
+            'start' => 'DESC',
+        ),
+        'meta_type'         => 'TIME'
+
+        ));
+
+
+        if ($sessions_query->have_posts()) :
+            while ($sessions_query->have_posts()) :
+                $sessions_query->the_post();
+                $date = get_field('date');
+                $eventdate = date("n-j-y", strtotime($date));
+                $tabs[$eventdate][get_the_title()] = [];
+            endwhile;
+            wp_reset_postdata();
+        endif;
+
         $the_query = new WP_Query(array(
         'post_type'         => 'proceedings',
         'posts_per_page'    => -1
         ));
-?>
-
-        <?php
 
         if ($the_query->have_posts()) :
             while ($the_query->have_posts()) :
@@ -53,53 +85,71 @@
                   'abstract'  => get_the_content()
                 ];
             endwhile;
+            wp_reset_postdata();
         endif;
 
-
-        // Sort tabs by date.
+// Sort tabs by date.
         ksort($tabs);
 
-        // Set up Day Tabs
-        echo '<div class="tabs"><ul class="schedule">';
+// Set up Day Tabs
+        ?>
+        <div class="tabs">
+        <ul class="schedule">
+
+        <?php
         foreach ($tabs as $day => $session) {
             $t = date_create_from_format("n-j-y", $day);
-            echo '<li class="accent background"><a class="accent color" href="#' . date_format($t, "n-j-y") . '">' . date_format($t, "M jS") . '</a></li>';
+            echo '<li class="accent background">
+              <a class="accent color" href="#' . date_format($t, "n-j-y") . '">' . date_format($t, "M jS") . '</a>
+            </li>';
         }
-        echo '</ul>';
+        ?>
+        </ul>
 
-        // Set up day blocks
+        <?php
+// Set up day blocks
         foreach ($tabs as $day => $session) {
+// Sort Sessions by start time and then by end time.
+
             $t = date_create_from_format("n-j-y", $day);
-            echo '<div id="' . date_format($t, "n-j-y") . '" class="session">';
-
-            // Sort Sessions by start time and then by end time.
-
-            foreach ($session as $key => $row) {
-                $startList[$key] = $row[0]['start'];
-                $endList[$key] = $row[0]['end'];
-            }
-            array_multisort($startList, SORT_ASC, $endList, SORT_ASC, $session);
-            unset($startList);
-            unset($endList);
-
-            // Set up session headers
+            ?>
+            <div id=
+            <?php echo '"' . date_format($t, "n-j-y") . '" class="session">'; ?>
+            <article>
+            <?php
             foreach ($session as $sess => $num) {
-                echo '<div class="title brand background"><h1 class="brand inverse"><span style="font-weight: bold;">' . $sess
-                . '</span> | ' . $num[0]['start'] . ' - ' . $num[0]['end'] . ' | Room ' . $num[0]['room']
-                . '</h1><div class="availability">' . $num[0]['avail'] . '</div></div>';
+// Set up session headers
+                if (!empty($num)) {
+                    //echo 'num' . var_dump($num);
+                    echo '<div class="title brand background">
+                      <h1 class="brand inverse">
+                        <span style="font-weight: bold;">' . $sess
+                        . '</span> | ' . $num[0]['start'] . ' - ' . $num[0]['end'] . ' | Room ' . $num[0]['room']
+                      . '</h1>
+                      <div class="availability">'
+                        . $num[0]['avail']
+                      . '</div>
+                    </div>';
 
-                // display post information
-                foreach ($num as $post) {
-                    echo '<div class="presentation"><h2><a class="accent color" href="' . $post['link'] . '">' . $post['title'] . '</a></h2>';
-                    echo '<p class="authors">';
-                    if ($post['speaker'] != '') {
-                        echo '<span style="font-weight: bold;">Speaker: </span>' . $post['speaker'] . ' | ';
+// display post information
+                    foreach ($num as $post) {
+                        echo '<div class="presentation">
+                          <h2>
+                            <a class="accent color" href="'; echo $post['link'] . '">' . $post['title'] . '</a>
+                          </h2>';
+                          echo '<p class="authors">';
+                        if ($post['speaker'] != '') {
+                            echo '<span style="font-weight: bold;">Speaker: </span>' . $post['speaker'] . ' | ';
+                        }
+                            echo '<span style="font-weight: bold;">Authors: </span> ' . $post['author']
+                          . '</p>
+                        </div>';
                     }
-                    echo '<span style="font-weight: bold;">Authors: </span> ' . $post['author'] . '</p></div>';
                 }
             }
-            echo '</div>';
-        }
-
-        echo '</div>';?>
+            ?>
+            </article>
+            </div>
+        <?php
+        } ?>
         </div>
